d6cfe379cff1c1912ef7a46f1c0a7d875041e2bf
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2020-08-04 10:06:29 +1000

    feat: Complete variables exercises

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,10 +1,11 @@
 // variables3.rs
-// Make me compile! Execute the command `rustlings hint variables3` if you want a hint :)
 
-// I AM NOT DONE
+// let is a constant. All variables are constant by default.
+// add a mut will make it a variable.
+// However, there is a const that is always a constant, and won't accept const
 
 fn main() {
-    let x = 3;
+    let mut x = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
--- exercises/variables/variables6.rs
+++ exercises/variables/variables6.rs
@@ -1,9 +1,19 @@
 // variables6.rs
-// Make me compile! Execute the command `rustlings hint variables6` if you want a hint :)
+// consts must always have a type annotation
+/*
+Length	Signed	Unsigned
+8-bit	i8	    u8
+16-bit	i16	    u16
+32-bit	i32	    u32
+64-bit	i64	    u64
+128-bit	i128	u128
 
-// I AM NOT DONE
+so i8 = -127 > 128. u8 = 0 - 256
+*/
 
-const NUMBER = 3;
+const NUMBER: i8 = 30;
+// const NUMBER: u8 = 256;
+
 fn main() {
     println!("Number {}", NUMBER);
 }
