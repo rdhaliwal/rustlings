c6f1fa6356e90dbc083638049a5720989ac0aa54
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2020-08-26 19:33:13 +1000

    feat: Complete tests exercises

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -6,12 +6,17 @@
 // This test has a problem with it -- make the test compile! Make the test
 // pass! Make the test fail! Execute `rustlings hint tests1` for hints :)
 
-// I AM NOT DONE
-
+// You define a modules for testing, and add this macro
 #[cfg(test)]
 mod tests {
+    // You prefix each test with this macro to indicate it's a unit test
     #[test]
     fn you_can_assert() {
-        assert!();
+        // Assert a boolean condition
+        assert!(10 > 1);
+
+        // Assert two values are equal
+        let thing = 10;
+        assert_eq!(thing, 10);
     }
 }
--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -4,10 +4,18 @@
 // we expect to get when we call `is_even(5)`.
 // Execute `rustlings hint tests3` for hints :)
 
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
+}
+
+pub fn divide_by_larger_number(a: u8, b: u8) -> u8 {
+    if b == 0 {
+        panic!("Divide by zero")
+    } else if a > b {
+        return b / a;
+    } else {
+        return a / b;
+    }
 }
 
 #[cfg(test)]
@@ -16,6 +24,12 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        assert!(is_even(4));
+    }
+
+    #[test]
+    #[should_panic]
+    fn this_test_panics() {
+        assert_eq!(divide_by_larger_number(10, 0), 1);
     }
 }
