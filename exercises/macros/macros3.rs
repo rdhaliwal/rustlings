d5926bfa46170bd9bae478a906920b0e5336a50c
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2020-08-27 18:01:00 +1000

    feat: Complete macros exercises

--- exercises/macros/macros3.rs
+++ exercises/macros/macros3.rs
@@ -2,9 +2,8 @@
 // Make me compile, without taking the macro out of the module!
 // Execute `rustlings hint macros3` for hints :)
 
-// I AM NOT DONE
-
-mod macros {
+#[macro_use]
+pub mod macros {
     macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
@@ -13,5 +12,6 @@ mod macros {
 }
 
 fn main() {
+    // Not sure how, but somehow the scope is happy WITHOUT using `macros`?
     my_macro!();
 }
--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -1,15 +1,16 @@
 // macros4.rs
 // Make me compile! Execute `rustlings hint macros4` for hints :)
 
-// I AM NOT DONE
-
+#[macro_use]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
-    ($val:expr) => {
+    }; // Separate each macro arm with a semi-color
+    // Have to use tt, not expr if i want to forward values on to another macro
+    // tt is token tree. More details here: https://doc.rust-lang.org/reference/macros-by-example.html
+    ($val:tt) => {
         println!("Look at this other macro: {}", $val);
-    }
+    };
 }
 
 fn main() {
